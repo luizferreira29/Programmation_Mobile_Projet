821335250c5aaaec61eb3b1ebf6a7a1d746218fe
Luiz <lippe.ferreira@yahoo.fr>
2020-05-17 02:12:12 +0200

    Test hors reseau et wifi

--- app/src/main/java/com/example/programmation_mobile_projet/MainActivity.java
+++ app/src/main/java/com/example/programmation_mobile_projet/MainActivity.java
@@ -4,12 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 
+import java.lang.reflect.Type;
+import java.nio.CharBuffer;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.jar.Attributes;
@@ -25,13 +30,37 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private ListAdapter ListAdapter;
     private RecyclerView.LayoutManager layoutManager;
+    private SharedPreferences Sharedpreferences;
+    private Gson gson;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        makeApiCall();
+        Sharedpreferences = getSharedPreferences("application_projet_app_mobile", Context.MODE_PRIVATE);
+        Gson gson = new GsonBuilder()
+                .setLenient()
+                .create();
+
+        List<Beer> BeerList = getDataFromCache();
+
+        if(BeerList != null){
+            showList(BeerList);
+        } else{
+            makeApiCall();
+    }
+    }
+
+    private List<Beer> getDataFromCache() {
+        String jsonBeer = Sharedpreferences.getString("jsonBeerList", null);
+
+        if(jsonBeer == null){
+            return null;
+        } else {
+            Type listType = new TypeToken<List<Beer>>() {}.getType();
+            return gson.fromJson(jsonBeer, listType);
+        }
     }
 
     private void showList(List<Beer> BeerList) {
@@ -48,10 +77,6 @@ public class MainActivity extends AppCompatActivity {
     private static final String BASE_URL = "https://api.punkapi.com/";
 
     private void makeApiCall(){
-        Gson gson = new GsonBuilder()
-                .setLenient()
-                .create();
-
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create(gson))
@@ -65,6 +90,7 @@ public class MainActivity extends AppCompatActivity {
             public void onResponse(Call<List<Beer>> call, Response<List<Beer>> response) {
                 if(response.isSuccessful() && response.body() !=null) {
                     List<Beer> BeerList = response.body();
+                    saveList(BeerList);
                     showList(BeerList);
                 } else
                     showError();
@@ -75,6 +101,15 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    private void saveList(List<Beer> BeerList) {
+        String jsonString = gson.toJson(BeerList);
+        Sharedpreferences
+                .edit()
+                .putString("JsonBeerList", "jsonString")
+                .apply();
+        Toast.makeText(getApplicationContext(), "List Saved", Toast.LENGTH_SHORT).show();
     }
 
     private void showError() {
